0dc0371e806a68904de21a41cca5f4a639bfc65f
sk789-sk <91289395+sk789-sk@users.noreply.github.com>
2023-03-14 22:58:46 -0400

    Delete Rosalind directory

--- Rosalind/Bioinformatics Stronghold/Infering_mRNA_from_protein.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 28 16:04:08 2021
-
-@author: Shams
-"""
-
--- Rosalind/untitled1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 28 23:22:36 2021
-
-@author: Shams
-"""
-
-n
